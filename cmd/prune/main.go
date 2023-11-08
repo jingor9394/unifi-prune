2440727026235bd32a109d02c6c4831f76a2ae9f
jingor9394 <>
2023-11-08 17:28:59 +0800

    input params

--- cmd/prune/main.go
+++ cmd/prune/main.go
@@ -1,64 +1,18 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"unifi-prune/config"
 	"unifi-prune/utils"
 )
 
 func main() {
-	var model string
-	var ip string
-	var user string
-	var password string
-	var port string
-
-	flag.StringVar(&model, "m", "", "")
-	flag.StringVar(&ip, "g", "", "")
-	flag.StringVar(&user, "u", "", "")
-	flag.StringVar(&port, "p", "", "")
-	flag.Usage = func() {
-		usage := `Usages:
--m <UDMPro/UDR/Controller>
-    Unifi model
--g
-    Unifi console ip address
--u
-    Unifi console user
--p
-    Unifi console password`
-		fmt.Println(usage)
-	}
-	flag.Parse()
-
-	if model == "" || ip == "" || user == "" {
-		flag.Usage()
-		return
-	}
-	models := map[string]bool{
-		config.ModelUDMPro:     true,
-		config.ModelUDR:        true,
-		config.ModelController: true,
-	}
-	if _, ok := models[model]; !ok {
-		flag.Usage()
-		return
-	}
-
-	fmt.Println("please enter password:")
-	_, err := fmt.Scanln(&password)
+	params, err := utils.NewParams()
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
-
-	run(model, ip, port, user, password)
-}
-
-func run(model, ip, port, user, password string) {
-	unifi := utils.NewUnifi(model, ip, port, user, password)
+	unifi := utils.NewUnifi(params.Model, params.IP, params.Port, params.User, params.Password)
 	defer unifi.Recover()
-	err := unifi.Prune()
+	err = unifi.Prune()
 	if err != nil {
 		fmt.Println(err)
 	}
--- utils/utils.go
+++ utils/utils.go
@@ -1,13 +1,74 @@
 package utils
 
-type InputParams struct {
-	Model string
-	IP    string
-	Port  string
-	User  string
-	Pass  string
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"unifi-prune/config"
+)
+
+type Params struct {
+	Model    string
+	IP       string
+	Port     string
+	User     string
+	Password string
 }
 
-func NewInputParams() (*InputParams, error) {
-	return &InputParams{}, nil
+var InvalidParams = errors.New("invalid params")
+
+func NewParams() (*Params, error) {
+	var model string
+	var ip string
+	var user string
+	var password string
+	var port string
+
+	flag.StringVar(&model, "m", "", "")
+	flag.StringVar(&ip, "g", "", "")
+	flag.StringVar(&user, "u", "", "")
+	flag.StringVar(&port, "p", "", "")
+	flag.Usage = func() {
+		usage := `Usages:
+-m <UDMPro/UDR/Controller>
+    Unifi model
+-g
+    Unifi console ip address
+-u
+    Unifi console user
+-p
+    Unifi console password`
+		fmt.Println(usage)
+	}
+	flag.Parse()
+
+	if model == "" || ip == "" || user == "" {
+		flag.Usage()
+		return nil, InvalidParams
+	}
+	models := map[string]bool{
+		config.ModelUDMPro:     true,
+		config.ModelUDR:        true,
+		config.ModelController: true,
+	}
+	if _, ok := models[model]; !ok {
+		flag.Usage()
+		return nil, InvalidParams
+	}
+
+	fmt.Println("please enter password:")
+	_, err := fmt.Scanln(&password)
+	if err != nil {
+		fmt.Println(err)
+		return nil, InvalidParams
+	}
+
+	params := &Params{
+		Model:    model,
+		IP:       ip,
+		Port:     port,
+		User:     user,
+		Password: password,
+	}
+	return params, nil
 }
